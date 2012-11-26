e5fd778c1309785715e27b52a9d4bd15cc881e8e
Zach Beane <xach@xach.com>
2012-11-25 21:37:34 -0500

    Update quicklisp.lisp user-agent version to 2012112500.

--- quicklisp.lisp
+++ quicklisp.lisp
@@ -968,7 +968,7 @@
                                   (format nil ":~D" port)))
       (add-line "Connection: close")
       ;; FIXME: get this version string from somewhere else.
-      (add-line "User-Agent: quicklisp-bootstrap/2011103100")
+      (add-line "User-Agent: quicklisp-bootstrap/2012112500")
       (add-newline sink)
       (sink-buffer sink))))
 
