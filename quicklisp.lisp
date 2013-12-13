b7051d9d342d1a2697148cc18b8755de13f4ac45
Zach Beane <xach@xach.com>
2013-12-12 21:06:23 -0500

    Add :errorp t LispWorks's OPEN-TCP-STREAM call.

--- quicklisp.lisp
+++ quicklisp.lisp
@@ -406,6 +406,7 @@
   (:implementation lispworks
     (qlqs-lispworks:open-tcp-stream host port
                                    :direction :io
+                                   :errorp t
                                    :read-timeout nil
                                    :element-type '(unsigned-byte 8)
                                    :timeout 5))
